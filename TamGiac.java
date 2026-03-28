12ea5dfeafc87c883163cb6f6521845cec6dba0b
itsmegojo123-hue <itsmegojo123@gmail.com>
2026-03-28 21:07:07 +0700

    Add files via upload

---
+++ TamGiac.java
@@ -0,0 +1,27 @@
+
+package tamgiac;
+
+
+import java.util.Scanner;
+
+public class TamGiac {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a, b, c;
+        System.out.print("- Nhap 3 canh a, b, c: ");
+
+        a = sc.nextInt();
+        b = sc.nextInt();
+        c = sc.nextInt();
+
+        
+        if (a + b > c && b + c > a && a + c > b) {
+            System.out.println("Day la tam giac");
+        } else {
+            System.out.println("Day khong phai la tam giac");
+        }
+
+        sc.close();
+    }
+}
