38e59b0d4893ccbcf6811c88b3ba7d038765a516
Ngọc Trâm <ngoctram1807200696@gmail.com>
2026-03-29 00:17:17 +0700

    Add TamGiacCan
    Thêm kiểm tra tam giác cân

---
+++ TamGiac.java
@@ -0,0 +1,31 @@
+
+package tamgiac;
+
+
+import java.util.Scanner;
+
+public class TamGiac {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a, b, c;
+        System.out.print("- Nhap 3 canh a, b, c: ");
+
+        a = sc.nextInt();
+        b = sc.nextInt();
+        c = sc.nextInt();
+
+        
+        if((a+b >c) && (a+c >b) && (b+c > a)){
+            System.out.println("Day la tam giac");
+            if(a==b || b==c || a==c){
+                if((a*a + b*b == c*c) || (a*a + c*c == b*b) || (b*b + c*c == a*a))
+                    System.out.println("-> Tam giac vuong can");
+                else
+                    System.out.println("-> Tam giac can");
+            }
+        }
+        else
+            System.out.println("-> Khong the tao thanh tam giac");
+    }
+}
